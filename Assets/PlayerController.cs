8c6ae5f9bff5aa1e16f88612bc61e8baa0ec72f0
LyyyyyyyyyyL <13079895734@163.com>
2024-11-04 23:03:21 +0000

    Initial commit or describe your changes

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,22 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
     public float mouseSensitivity = 100f;
+    public float jumpForce = 5f;
+    public float gravty = -19.6f;
 
     private float xRotation = 0f;
+    private bool isGrounded = true; // 是否在地面上
+    private Rigidbody rb;
 
-        // Start is called before the first frame update
-        void Start()
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
+        Physics.gravity = new Vector3(0, gravty, 0);
     }
 
-        void Update()
+    void Update()
     {
         // 角色移动
         float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
@@ -35,6 +38,14 @@ public class PlayerController : MonoBehaviour
 
         Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         transform.Rotate(Vector3.up * mouseX);
+
+        // 跳跃
+        if (Input.GetKeyDown(KeyCode.Space) )
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false; // 跳跃后设置为不在地面上
+        }
+
+
     }
 }
-
